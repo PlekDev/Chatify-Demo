701577f6aefd219f0a38d97d699f0824cea12283
user971038 <A00226997@tec.mx>
2026-04-25 02:23:21 -0600

    ay, olvidé quitar cosas del de users

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,18 +1,15 @@
 import './../App.css'
 
-const Users = () => {
-    // Si hay una manera de verificar los users activos -> 
-    // const Users = ({ userList = [] }) => { 
-    //if (userList.length === 0) return <p>No active users.</p>;
+const Users = ({ userList = [] }) => { 
+    if (userList.length === 0) return <p>No active users.</p>;
 
     return (
         <div className="container" id="users-container">
-            {/*{userList.map((user) => (*/}
-                <div className="users"> {/* key={user.id} */}
-                    {/*<span>{user.name}</span>*/}
-                    <span>Rosita Fresita</span>
+            {userList.map((user) => (
+                <div className="users" key={user.id}>
+                    <span>{user.name}</span>
                 </div>
-            {/*))}(*/}
+            ))}
         </div>
     );
 };
